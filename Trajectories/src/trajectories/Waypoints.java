e687c474ed51074e5b15070c593ba13556b88d12
Kaito Arai <Kaito Arai@10.26.3.78>
2017-01-16 17:41:07 +0000

    Trajectory Project for generating all necessary trajectories

---
+++ Trajectories/src/trajectories/Waypoints.java
@@ -0,0 +1,15 @@
+package trajectories;
+
+import jaci.pathfinder.Waypoint;
+
+public class Waypoints {
+
+	// aw - alliance wall
+	public static Waypoint RED_AW_1, RED_AW_2, RED_AW_3, BLUE_AW_1, BLUE_AW_2, BLUE_AW_3;
+
+	// as - airshop
+	public static Waypoint RED_AS_1, RED_AS_2, RED_AS_3, BLUE_AS_1, BLUE_AS_2, BLUE_AS_3;
+
+	// o - overflow, r - return, ls - loading station
+	public static Waypoint RED_OLS, RED_RLS, BLUE_OLS, BLUE_RLS;
+}
