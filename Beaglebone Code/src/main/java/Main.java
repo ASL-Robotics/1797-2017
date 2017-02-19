13552f5d77b3eaea1c825ea17fe377b38decb42d
kaitoa19 <kaito_arai@asl.org>
2017-02-19 08:52:48 +0000

    Added a vertical line

--- Beaglebone Code/src/main/java/Main.java
+++ Beaglebone Code/src/main/java/Main.java
@@ -12,167 +12,178 @@ import org.opencv.core.Point;
 import com.google.gson.Gson;
 
 public class Main {
-  public static ArrayList<MatOfPoint> contours;
-  public static ArrayList<Rect> targetRects;
-  public static Gson gson;
-  
-  public static void main(String[] args) {
-    // Loads our OpenCV library. This MUST be included
-    System.loadLibrary("opencv_java310");
+	public static ArrayList<MatOfPoint> contours;
+	public static ArrayList<Rect> targetRects;
+	public static Gson gson;
 
-    // Connect NetworkTables, and get access to the publishing table
-    NetworkTable.setClientMode();
-    // Set your team number here
-    NetworkTable.setTeam(1797);
+	public static void main(String[] args) {
+		// Loads our OpenCV library. This MUST be included
+		System.loadLibrary("opencv_java310");
 
-    NetworkTable.initialize();
-    
-    contours = new ArrayList<MatOfPoint>();
-    targetRects = new ArrayList<Rect>();
-    gson = new Gson();
+		// Connect NetworkTables, and get access to the publishing table
+		NetworkTable.setClientMode();
+		// Set your team number here
+		NetworkTable.setTeam(1797);
 
-    // This is the network port you want to stream the raw received image to
-    // By rules, this has to be between 1180 and 1190, so 1185 is a good choice
-    int streamPort = 1185;
+		NetworkTable.initialize();
 
-    // This stores our reference to our mjpeg server for streaming the input image
-    MjpegServer inputStream = new MjpegServer("MJPEG Server", streamPort);
+		contours = new ArrayList<MatOfPoint>();
+		targetRects = new ArrayList<Rect>();
+		gson = new Gson();
 
-    // Selecting a Camera
-    // Uncomment one of the 2 following camera options
-    // The top one receives a stream from another device, and performs operations based on that
-    // On windows, this one must be used since USB is not supported
-    // The bottom one opens a USB camera, and performs operations on that, along with streaming
-    // the input image so other devices can see it.
+		// This is the network port you want to stream the raw received image to
+		// By rules, this has to be between 1180 and 1190, so 1185 is a good
+		// choice
+		int streamPort = 1185;
 
-    // HTTP Camera
-    
-    // This is our camera name from the robot. this can be set in your robot code with the following command
-    // CameraServer.getInstance().startAutomaticCapture("YourCameraNameHere");
-    // "USB Camera 0" is the default if no string is specified
-    String cameraName = "FRONT";
-    HttpCamera camera;
-    while ((camera = setHttpCamera(cameraName, inputStream)) == null) {}
-    // It is possible for the camera to be null. If it is, that means no camera could
-    // be found using NetworkTables to connect to. Create an HttpCamera by giving a specified stream
-    // Note if this happens, no restream will be created
-    
-    //if (camera == null) {
-    //  camera = new HttpCamera("CoprocessorCamera", "URL");
-    //  inputStream.setSource(camera);
-    //}
-    
-    
-      
+		// This stores our reference to our mjpeg server for streaming the input
+		// image
+		MjpegServer inputStream = new MjpegServer("MJPEG Server", streamPort);
 
-    /***********************************************/
+		// Selecting a Camera
+		// Uncomment one of the 2 following camera options
+		// The top one receives a stream from another device, and performs
+		// operations based on that
+		// On windows, this one must be used since USB is not supported
+		// The bottom one opens a USB camera, and performs operations on that,
+		// along with streaming
+		// the input image so other devices can see it.
 
-    // USB Camera
-    /*
-    // This gets the image from a USB camera 
-    // Usually this will be on device 0, but there are other overloads
-    // that can be used
-    UsbCamera camera = setUsbCamera(0, inputStream);
-    // Set the resolution for our camera, since this is over USB
-    camera.setResolution(640,480);
-    */
+		// HTTP Camera
 
-    GripPipeline pipeline = new GripPipeline();
+		// This is our camera name from the robot. this can be set in your robot
+		// code with the following command
+		// CameraServer.getInstance().startAutomaticCapture("YourCameraNameHere");
+		// "USB Camera 0" is the default if no string is specified
+		String cameraName = "FRONT";
+		HttpCamera camera;
+		while ((camera = setHttpCamera(cameraName, inputStream)) == null) {
+		}
+		// It is possible for the camera to be null. If it is, that means no
+		// camera could
+		// be found using NetworkTables to connect to. Create an HttpCamera by
+		// giving a specified stream
+		// Note if this happens, no restream will be created
 
-    // This creates a CvSink for us to use. This grabs images from our selected camera, 
-    // and will allow us to use those images in opencv
-    CvSink imageSink = new CvSink("CV Image Grabber");
-    imageSink.setSource(camera);
+		// if (camera == null) {
+		// camera = new HttpCamera("CoprocessorCamera", "URL");
+		// inputStream.setSource(camera);
+		// }
 
-    // This creates a CvSource to use. This will take in a Mat image that has had OpenCV operations
-    // operations 
-    CvSource imageSource = new CvSource("CV Image Source", VideoMode.PixelFormat.kMJPEG, 160, 90, 30);
-    MjpegServer cvStream = new MjpegServer("CV Image Stream", 1186);
-    cvStream.setSource(imageSource);
+		/***********************************************/
 
-    // All Mats and Lists should be stored outside the loop to avoid allocations
-    // as they are expensive to create
-    Mat inputImage = new Mat();
+		// USB Camera
+		/*
+		 * // This gets the image from a USB camera // Usually this will be on
+		 * device 0, but there are other overloads // that can be used UsbCamera
+		 * camera = setUsbCamera(0, inputStream); // Set the resolution for our
+		 * camera, since this is over USB camera.setResolution(640,480);
+		 */
 
-    // Infinitely process image
-    while (true) {
-      // Grab a frame. If it has a frame time of 0, there was an error.
-      // Just skip and continue
-      long frameTime = imageSink.grabFrame(inputImage);
-      if (frameTime == 0) continue;
+		GripPipeline pipeline = new GripPipeline();
 
-      // Below is where you would do your OpenCV operations on the provided image
-      // The sample below just changes color source to HSV
-      //Imgproc.cvtColor(inputImage, hsv, Imgproc.COLOR_RGB2HSV);
-      pipeline.process(inputImage);
-      // Here is where you would write a processed image that you want to restreams
-      // This will most likely be a marked up image of what the camera sees
-      // For now, we are just going to stream the HSV image
-      
-      contours = pipeline.filterContoursOutput();
+		// This creates a CvSink for us to use. This grabs images from our
+		// selected camera,
+		// and will allow us to use those images in opencv
+		CvSink imageSink = new CvSink("CV Image Grabber");
+		imageSink.setSource(camera);
 
-      targetRects.clear();
+		// This creates a CvSource to use. This will take in a Mat image that
+		// has had OpenCV operations
+		// operations
+		CvSource imageSource = new CvSource("CV Image Source", VideoMode.PixelFormat.kMJPEG, 160, 90, 30);
+		MjpegServer cvStream = new MjpegServer("CV Image Stream", 1186);
+		cvStream.setSource(imageSource);
 
-      for (int i = 0; i < contours.size(); i++) {
-        targetRects.add(Imgproc.boundingRect(contours.get(i)));
-        // System.out.print(targetRects.get(i).x + " ");
-      }
-      
-      NetworkTable.getTable("Vision").putString("Target Rectangles", gson.toJson(targetRects.toArray()));
-      
-      // debug code
-      
-      for(Rect rect : targetRects) {
-        Imgproc.rectangle(inputImage, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height),new Scalar(255, 255, 255));
-      }
-      imageSource.putFrame(inputImage);
-      //System.out.println(pipeline.filterContoursOutput().size());
-    }
-  }
+		// All Mats and Lists should be stored outside the loop to avoid
+		// allocations
+		// as they are expensive to create
+		Mat inputImage = new Mat();
 
-  private static HttpCamera setHttpCamera(String cameraName, MjpegServer server) {
-    // Start by grabbing the camera from NetworkTables
-    NetworkTable publishingTable = NetworkTable.getTable("CameraPublisher");
-    // Wait for robot to connect. Allow this to be attempted indefinitely
-    while (true) {
-      try {
-        if (publishingTable.getSubTables().size() > 0) {
-          break;
-        }
-        Thread.sleep(500);
-        } catch (Exception e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-    }
+		// Infinitely process image
+		while (true) {
+			// Grab a frame. If it has a frame time of 0, there was an error.
+			// Just skip and continue
+			long frameTime = imageSink.grabFrame(inputImage);
+			if (frameTime == 0)
+				continue;
 
+			// Below is where you would do your OpenCV operations on the
+			// provided image
+			// The sample below just changes color source to HSV
+			// Imgproc.cvtColor(inputImage, hsv, Imgproc.COLOR_RGB2HSV);
+			pipeline.process(inputImage);
+			// Here is where you would write a processed image that you want to
+			// restreams
+			// This will most likely be a marked up image of what the camera
+			// sees
+			// For now, we are just going to stream the HSV image
 
-    HttpCamera camera = null;
-    if (!publishingTable.containsSubTable(cameraName)) {
-      return null;
-    }
-    ITable cameraTable = publishingTable.getSubTable(cameraName);
-    String[] urls = cameraTable.getStringArray("streams", null);
-    if (urls == null) {
-      return null;
-    }
-    ArrayList<String> fixedUrls = new ArrayList<String>();
-    for (String url : urls) {
-      if (url.startsWith("mjpg")) {
-        fixedUrls.add(url.split(":", 2)[1]);
-      }
-    }
-    camera = new HttpCamera("CoprocessorCamera", fixedUrls.toArray(new String[0]));
-    server.setSource(camera);
-    return camera;
-  }
+			contours = pipeline.filterContoursOutput();
 
-  private static UsbCamera setUsbCamera(int cameraId, MjpegServer server) {
-    // This gets the image from a USB camera 
-    // Usually this will be on device 0, but there are other overloads
-    // that can be used
-    UsbCamera camera = new UsbCamera("CoprocessorCamera", cameraId);
-    server.setSource(camera);
-    return camera;
-  }
+			targetRects.clear();
+
+			for (int i = 0; i < contours.size(); i++) {
+				targetRects.add(Imgproc.boundingRect(contours.get(i)));
+				// System.out.print(targetRects.get(i).x + " ");
+			}
+
+			NetworkTable.getTable("Vision").putString("Target Rectangles", gson.toJson(targetRects.toArray()));
+
+			// debug code
+
+			for (Rect rect : targetRects) {
+				Imgproc.rectangle(inputImage, new Point(rect.x, rect.y),
+						new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(255, 255, 255));
+				Imgproc.line(inputImage, new Point(44, 0), new Point(44, 160), new Scalar(255, 255, 255), 1, 8, 0);
+			}
+			imageSource.putFrame(inputImage);
+			// System.out.println(pipeline.filterContoursOutput().size());
+		}
+	}
+
+	private static HttpCamera setHttpCamera(String cameraName, MjpegServer server) {
+		// Start by grabbing the camera from NetworkTables
+		NetworkTable publishingTable = NetworkTable.getTable("CameraPublisher");
+		// Wait for robot to connect. Allow this to be attempted indefinitely
+		while (true) {
+			try {
+				if (publishingTable.getSubTables().size() > 0) {
+					break;
+				}
+				Thread.sleep(500);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		HttpCamera camera = null;
+		if (!publishingTable.containsSubTable(cameraName)) {
+			return null;
+		}
+		ITable cameraTable = publishingTable.getSubTable(cameraName);
+		String[] urls = cameraTable.getStringArray("streams", null);
+		if (urls == null) {
+			return null;
+		}
+		ArrayList<String> fixedUrls = new ArrayList<String>();
+		for (String url : urls) {
+			if (url.startsWith("mjpg")) {
+				fixedUrls.add(url.split(":", 2)[1]);
+			}
+		}
+		camera = new HttpCamera("CoprocessorCamera", fixedUrls.toArray(new String[0]));
+		server.setSource(camera);
+		return camera;
+	}
+
+	private static UsbCamera setUsbCamera(int cameraId, MjpegServer server) {
+		// This gets the image from a USB camera
+		// Usually this will be on device 0, but there are other overloads
+		// that can be used
+		UsbCamera camera = new UsbCamera("CoprocessorCamera", cameraId);
+		server.setSource(camera);
+		return camera;
+	}
 }
